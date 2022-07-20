aa55a9cc33f86e4b31a34accceddb01649d5d0ca
Benji Vesterby <benji@devnw.com>
2022-07-19 20:42:22 -0400

    Adding go generate for test data

--- regex_test.go
+++ regex_test.go
@@ -8,6 +8,24 @@ import (
 	"go.devnw.com/event"
 )
 
+// Steven Black Block List: https://github.com/mmotti/pihole-regex
+//go:generate curl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -o ./testdata/remote/stevenblack_block.hosts
+
+// Regex Block List and False-Positive Allow List: https://github.com/mmotti/pihole-regex
+//go:generate curl https://raw.githubusercontent.com/mmotti/pihole-regex/master/regex.list -o ./testdata/remote/mmotti_block.regex
+//go:generate curl https://github.com/mmotti/pihole-regex/blob/master/whitelist.list -o ./testdata/remote/mmotti_allow.direct
+
+// Firebog List of Lists: https://firebog.net/
+//go:generate curl https://v.firebog.net/hosts/lists.php?type=all -o ./testdata/remote/firebog_block.lists
+
+// Developer Dan Lists: https://github.com/lightswitch05/hosts
+//go:generate curl https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt -o ./testdata/remote/ddanads_block.hosts
+//go:generate curl https://www.github.developerdan.com/hosts/lists/facebook-extended.txt -o ./testdata/remote/ddanfb_block.hosts
+//go:generate curl https://www.github.developerdan.com/hosts/lists/amp-hosts-extended.txt -o ./testdata/remote/ddanamp_block.hosts
+//go:generate curl https://www.github.developerdan.com/hosts/lists/dating-services-extended.txt -o ./testdata/remote/ddandating_block.hosts
+//go:generate curl https://www.github.developerdan.com/hosts/lists/hate-and-junk-extended.txt -o ./testdata/remote/ddanhatejunk_block.hosts
+//go:generaet curl https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt -o ./testdata/remote/ddantracking_block.hosts
+
 func Test_Match(t *testing.T) {
 	tests := map[string]struct {
 		regex   string
