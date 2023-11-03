2952c01a5161c3287d32b06c8c1774ce24501414
Benji Vesterby <benji@devnw.com>
2023-11-02 20:59:16 -0400

    fix: correct postinstall script issue
    fixes #44

--- deployment/scripts/postinstall.sh
+++ deployment/scripts/postinstall.sh
@@ -3,12 +3,18 @@ set -e
 
 configure()
 {
-	# Set DNSStubListener=no in /etc/systemd/resolved.conf
-	sed -i 's/#DNSStubListener=yes/DNSStubListener=no/' /etc/systemd/resolved.conf
+	# Check if /etc/systemd/resolved.conf exists before attempting to modify it
+	if [ -f /etc/systemd/resolved.conf ]; then
+		# Set DNSStubListener=no in /etc/systemd/resolved.conf
+		sed -i 's/#DNSStubListener=yes/DNSStubListener=no/' /etc/systemd/resolved.conf
+	fi
 
-	# Restart the systemd-resolved service
-	systemctl restart systemd-resolved
-
+	# Check if the systemd-resolved service exists and is active before attempting to restart it
+	if systemctl is-active --quiet systemd-resolved; then
+		# Restart the systemd-resolved service
+		systemctl restart systemd-resolved
+	fi
+	
 	systemctl enable void.service
 	
 	systemctl daemon-reload
